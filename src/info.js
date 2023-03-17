2f4638a6b956fc797a664e1bac4f4e8f724f04f0
Harrison Anthony <harrisoneanthony@gmail.com>
2023-03-16 21:03:58 -0400

    messing with props

--- src/info.js
+++ src/info.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { propTypes } from "prop-types";
 
 class Info extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log(props);
+    }
+
     render(){
-        const title = "This is my title.";
+        const title = this.props.title;
         const showTitle = true;
 
         if (showTitle){
@@ -18,5 +24,12 @@ class Info extends React.Component {
     }
 }
 
+Info.defaultProps = {
+    title: "Default"
+}
+
+Info.propTypes = {
+    title: propTypes.string
+}
 
 export default Info;
