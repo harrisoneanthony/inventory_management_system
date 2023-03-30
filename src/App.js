1c160d785f66b783661c05354aaf781a7e0dd427
Harrison Anthony <harrisoneanthony@gmail.com>
2023-03-29 22:17:21 -0400

    setting up inventory app search bar functionality

--- src/App.js
+++ src/App.js
@@ -1,69 +1,21 @@
-// import logo from './logo.svg';
 import './App.css';
-import Info from "./info.js";
-// import { ProfilerProps } from 'react';
-// import { propTypes } from "prop-types";
-import { useState } from "react";
-
+import SearchBar from "./SearchBar"
+import {useState} from "react"
 
 function App() {
+  const [data, setData] = useState({});
+
+  const updateData = (searchParams) => {
+    setData(searchParams);
+  }
+
   return (
     <div className="App">
-        <Info/>
-        <ButtonState/>
-        {/* <AddItem text="Harrison" number={2}/>
-        <AddItem text="some"/>
-        <AddItem/> */}
-    </div>
-  );
-}
-
-
-// function AddItem(props){
-//   return(
-//     <form>
-//       <label for="text-form">Type something:</label>
-//       <input type="text" value={props.text} id="text-form"></input>
-//       <p>{props.number}</p>
-//     </form>
-//   );
-// }
-
-// AddItem.defaultProps = {
-//   number: 1,
-// };
-
-// AddItem.propTypes = {
-//   number: propTypes.number,
-//   title: propTypes.string
-// }
-
-function ButtonState(){
-  const [title, setTitle ] = useState("");
-  const [count, setCount] = useState(0);
-  
-  const updateTitleClicked = () => {
-    setTitle("We now have a title!");
-  }
-
-  const updateCounterClicked = () => {
-    setCount(count + 1);
-  }
-
-  return (
-    <div>
-      <Data title={title} count={count} />
-      <button onClick={updateTitleClicked}>Update Title</button>
-      <button onClick={updateCounterClicked}>Update Counter</button>
-    </div>
-  )
-}
-
-function Data(props){
-  return(
-    <div>
-      <p>Title: {props.title}</p>
-      <p>Count: {props.count}</p>
+      <SearchBar callback={updateData}/>
+      <p> Name: { "name" in data ? data["name"] : "No Data to display" }</p>
+      <p> Max Price: { "price" in data ? data["price"] : "No Data to display" }</p>
+      <p> Type: { "type" in data ? data["type"] : "No Data to display" }</p>
+      <p> Brand: { "brand" in data ? data["brand"] : "No Data to display" }</p>
     </div>
   );
 }
