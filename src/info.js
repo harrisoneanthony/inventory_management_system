b4d476b4b49215abdaa449a7e9efbd720983b60c
Harrison Anthony <harrisoneanthony@gmail.com>
2023-03-29 21:23:57 -0400

    props, proptypes, state

--- src/info.js
+++ src/info.js
@@ -1,18 +1,51 @@
-function Info(){
-    const title = "This is my title"
-    const showTitle = true;
+import React from "react";
+// import { propTypes } from "prop-types";
 
-if(showTitle){
-
-    return (
-        <div>
-        <h1>{showTitle ? title : ""}</h1>
-        <p>Mannage your stuff.</p>
-    </div>
-    );
-    } else {
-        return <p>empty</p>
+class Info extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            count: 0,
+        };
+        this.buttonPressed = this.buttonPressed.bind(this);
     }
-}
+
+    buttonPressed(){
+        this.setState({
+            count: this.state.count + 1,
+        });
+    }
+
+    render(){
+        return(
+            <div>
+                {" "}
+                <p>Count: {this.state.count}</p>
+                <button onClick={this.buttonPressed}>Click me!</button>
+            </div>
+        )
+        // const title= this.props.title;
+        // const showTitle = true;
+
+        // if(showTitle){
+        //     return (
+        //         <div>
+        //         <h1>{showTitle ? title : ""}</h1>
+        //         <p>Mannage your stuff.</p>
+        //     </div>
+        //     );
+        // } else {
+        //     return <p>empty</p>
+        // }
+    }
+}    
+
+// Info.defaultProps = {
+//     title: "Default"
+// }
+
+// Info.propTypes = {
+//     title: propTypes.string,
+// }
 
 export default Info
